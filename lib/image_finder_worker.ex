c7d733faad8b1afe81ee6dc9285f1e474a52ec57
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-05-23 16:08:43 -0300

    Using digest as name

--- lib/image_finder_worker.ex
+++ lib/image_finder_worker.ex
@@ -10,17 +10,23 @@ defmodule ImageFinder.Worker do
   end
 
   def handle_call({:fetch, source_file, target_directory}, _from, state) do
-    content = File.read source_file
+    content = File.read! source_file
     regexp = ~r/http(s?)\:.*?\.(png|jpg|gif)/
     Regex.scan(regexp, content)
       |> Enum.map(&List.first/1)
-      |> Enum.map(&(download_into &1, target_directory))
-
+      |> Enum.map(&(fetch_link &1, target_directory))
     {:reply, :ok, state}
   end
 
-  def download_into(url, directory) do
-    result = HTTPotion.get url
-    File.write directory, url
+  def fetch_link(link, target_directory) do
+    HTTPotion.get(link).body  |> save(target_directory)
+  end
+
+  def digest(body) do
+    :crypto.hash(:md5 , body) |> Base.encode16()
+  end
+
+  def save(body, directory) do
+    File.write! "#{directory}/#{digest(body)}", body
   end
 end
